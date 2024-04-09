33d6df37ff91a917ef1cced008073aa6dda82025
devodii <emmanuelodii80@gmail.com>
2024-04-10 00:02:55 +0100

    chore: setup config module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const configService = new ConfigService();
+  const port = configService.get<number>('PORT');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
