8883f182a0e288deb89036a4acc25b141360cc10
devodii <emmanuelodii80@gmail.com>
2024-04-13 15:32:43 +0100

    chore: add auth middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,27 @@
+import { getUser } from "@/actions/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+
+  const user = await getUser();
+
+  if (!user?.id && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/?auth=signIn", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+  ],
+};
